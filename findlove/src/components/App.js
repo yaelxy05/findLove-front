552e4c1b9ef94aac7fab626bb495b2285fee1728
yaelx <yaelx@localhost>
2022-10-04 16:54:18 +0200

    modify architecture css

--- findlove/src/components/App.js
+++ findlove/src/components/App.js
@@ -1,10 +1,10 @@
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      
     </div>
   );
 }
--- findlove/src/index.js
+++ findlove/src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 
-import "./index.css";
+import "./styles/index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 
