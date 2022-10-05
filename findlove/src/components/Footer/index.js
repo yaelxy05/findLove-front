c307ed189ae29f83bab53c3c08b3900bd6c913a3
yaelx <yaelx@localhost>
2022-10-05 12:24:37 +0200

    add component footer

---
+++ findlove/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Copyright from "./copyright";
+function Footer() {
+  return (
+    <footer className="footer">
+      <Copyright />
+    </footer>
+  );
+}
+
+export default Footer;
